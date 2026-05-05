7fcd73452dd301d93bde68fbcef4d4c66c31d803
luth0910 <m.ruhunul.luthfi@gmail.com>
2026-05-05 23:10:28 +0700

    test: Add constants for login automation in OrangeHRM

---
+++ cypress/e2e/tugas-17/orange-hrm-login-auto-with-pom.cy.js
@@ -0,0 +1,4 @@
+const BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+const DASHBOARD_URL = "/web/index.php/dashboard/index";
+const VALID_USERNAME = "Admin";
+const VALID_PASSWORD = "admin123";
